bde310a1050e3d3cd5eb6cdd337c8d3e33d30bd1
Dilshan Danushka <dilshan@convertium.com>
2022-10-28 02:15:34 +0530

    get data from the api

---
+++ src/services/data.js
@@ -0,0 +1,26 @@
+import axios from 'axios';
+
+const getData = async (url) => {
+    const { data } = await axios.get(url);
+    return data;
+};
+
+// get all shows
+export function AllShows() {
+    return getData(`https://api.tvmaze.com/shows`);
+}
+
+// get specific show
+export function getShow(key) {
+    return getData(`https://api.tvmaze.com/shows/${key}`);
+}
+
+// get single recipe
+export function getEpisodes(key) {
+    return getData(`https://api.tvmaze.com/shows/${key}/episodes`);
+}
+
+// search for recipes
+export function getSearchResult(keyword) {
+    return getData(`https://api.tvmaze.com/search/shows?q=${keyword}`);
+}
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,14 +1,24 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <div>
+
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
+import { getShow } from '@/services/data';
 
+getShow(10000).then(
+  res => {
+    console.log(res)
+  }).catch((err) => {
+    console.log("Couldn't find the show you're looking for, " + err.message)
+  })
 export default {
   name: 'HomeView',
   components: {
