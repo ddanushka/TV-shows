3ebfbc338d44162ecae23ecdaefda28648809179
Dilshan Danushka <dilshan@convertium.com>
2022-10-30 22:27:11 +0530

    Start icon added to ratings

--- src/components/ListItem.vue
+++ src/components/ListItem.vue
@@ -5,7 +5,7 @@
       <div class="genre-tags">
         <span v-for="genre in genres" :key="genre">{{genre}}</span>
       </div>
-      <div class="rating">{{rating}}</div>
+      <div class="rating">&#9733; {{rating}}</div>
       <img v-if="imgUrl" :src="imgUrl" />
       <img v-else src="@/assets/images/no-image.jpeg" />
     </router-link>
